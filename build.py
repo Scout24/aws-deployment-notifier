c59aff7074e8b585d0f5509c4b5ae3d280acd514
Konrad Hosemann <konrad@hosemann.name>
2015-05-21 14:30:14 +0200

    reenable some deps, dep resolving fixed on TC

--- build.py
+++ build.py
@@ -3,11 +3,11 @@ from pybuilder.core import use_plugin, init, Author
 use_plugin('python.core')
 use_plugin('python.install_dependencies')
 use_plugin('python.distutils')
-#use_plugin('python.flake8')
+use_plugin('python.flake8')
 use_plugin('python.unittest')
-#use_plugin('python.coverage')
+use_plugin('python.coverage')
 use_plugin('copy_resources')
-#use_plugin('python.pytddmon')
+use_plugin('python.pytddmon')
 
 default_task = ['analyze', 'publish']
 
