f151e9a15a554943a02933f93dd53825bfc3002e
Konrad Hosemann <konrad@hosemann.name>
2015-07-15 14:57:21 +0200

    fix tests

--- src/unittest/python/notifier_tests.py
+++ src/unittest/python/notifier_tests.py
@@ -8,11 +8,17 @@ class NotifierTest(unittest2.TestCase):
     def test_parameters_are_submitted(self, connect_to_region_mock):
         topic = "abc"
         region = "eu-west-2"
-        notifier = dnot.Notifier(sns_region=region)
+        result_topic = "result"
+        stack_name = "stack1"
+        params = '{"key": "value"}'
 
-        notifier.publish(sns_topic_arn=topic, stack_name="stack1", params='{ "key": "value" }')
+        notifier = dnot.Notifier(sns_region=region)
+        notifier.publish(sns_topic_arn=topic, stack_name=stack_name, result_topic=result_topic, params=params)
 
         connect_to_region_mock.assert_called_with(region)
         connect_to_region_mock.return_value.publish.assert_called_with(
             topic=topic,
-            message='{"stackName": "stack1", "region": "eu-west-1", "params": {"key": "value"}}')
+            message='{{"stackName": "{0}", "notificationARN": "{1}", "region": "eu-west-1", "params": {2}}}'.format(
+                stack_name,
+                result_topic,
+                params))
